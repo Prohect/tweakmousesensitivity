f61f860f4e38faad398c2bc8cb0d23fb4ee09043
prohect <prohect@foxmail.com>
2025-01-07 02:39:50 +0800

    init commit

---
+++ src/client/java/com/github/prohect/mixin/client/MixinMouse.java
@@ -0,0 +1,53 @@
+package com.github.prohect.mixin.client;
+
+import net.minecraft.client.Mouse;
+import org.spongepowered.asm.mixin.Mixin;
+
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Mouse.class)
+public class MixinMouse {
+
+    @Inject(at = @At("HEAD"), method = "updateMouse", cancellable = true)
+    private void updateMouse(double timeDelta, CallbackInfo ci) {
+        try {
+            var that = (Mouse) (Object) this;
+            Double value = that.client.options.getMouseSensitivity().getValue();
+            double d = value * 0.6F + 0.2F;
+            double e = d * d * d * (value + Double.MIN_NORMAL);
+            double f = e * 8.0;
+            double i;
+            double j;
+            if (that.client.options.smoothCameraEnabled) {
+                double g = that.cursorXSmoother.smooth(that.cursorDeltaX * f, timeDelta * f);
+                double h = that.cursorYSmoother.smooth(that.cursorDeltaY * f, timeDelta * f);
+                i = g;
+                j = h;
+            } else if (that.client.options.getPerspective().isFirstPerson() && that.client.player.isUsingSpyglass()) {
+                that.cursorXSmoother.clear();
+                that.cursorYSmoother.clear();
+                i = that.cursorDeltaX * e;
+                j = that.cursorDeltaY * e;
+            } else {
+                that.cursorXSmoother.clear();
+                that.cursorYSmoother.clear();
+                i = that.cursorDeltaX * f;
+                j = that.cursorDeltaY * f;
+            }
+
+            int k = 1;
+            if (that.client.options.getInvertYMouse().getValue()) {
+                k = -1;
+            }
+
+            that.client.getTutorialManager().onUpdateMouse(i, j);
+            if (that.client.player != null) {
+                that.client.player.changeLookDirection(i, j * (double) k);
+            }
+        } finally {
+            ci.cancel();
+        }
+    }
+}
--- src/main/java/com/github/prohect/ExampleMod.java
+++ src/main/java/com/github/prohect/ExampleMod.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.github.prohect;
 
 import net.fabricmc.api.ModInitializer;
 
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
+	public static final String MOD_ID = "tweakmousesensitivity";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
@@ -19,6 +19,5 @@ public class ExampleMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
 	}
 }
