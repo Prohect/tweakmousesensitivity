ac242a57ab4459bdee31c87aafdefcf68d699dc2
prohect <prohect@foxmail.com>
2025-01-13 01:16:18 +0800

    init commit

--- src/client/java/com/github/TweakMouseSensitivityClient.java
+++ src/client/java/com/github/TweakMouseSensitivityClient.java
@@ -1,9 +1,8 @@
-package com.github.prohect;
+package com.github;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
-
+public class TweakMouseSensitivityClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
--- src/main/java/com/github/TweakMouseSensitivity.java
+++ src/main/java/com/github/TweakMouseSensitivity.java
@@ -1,12 +1,12 @@
-package com.github.prohect;
+package com.github;
 
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "tweakmousesensitivity";
+public class TweakMouseSensitivity implements ModInitializer {
+	public static final String MOD_ID = "tweak-mouse-sensitivity";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
@@ -19,5 +19,6 @@ public class ExampleMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
+//		LOGGER.info("Hello Fabric world!");
 	}
 }
